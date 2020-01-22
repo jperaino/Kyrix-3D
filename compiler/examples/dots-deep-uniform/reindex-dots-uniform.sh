fc74be48f4fd4c66d8356e264be731a11aa4325b
Peter Griggs <pgriggs@mit.edu>
2020-01-22 12:30:52 -0500

    debugging dots deep uniform

--- compiler/examples/dots-deep-uniform/reindex-dots-uniform.sh
+++ compiler/examples/dots-deep-uniform/reindex-dots-uniform.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # wrapped as a script for start-kyrix.sh
 
-cd /kyrix/compiler/examples/dots-uniform
+cd /kyrix/compiler/examples/dots-deep-uniform
 node dots.js | egrep -i "error|connected" || true
